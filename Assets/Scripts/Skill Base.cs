13ec0f0727e62cc9e3fdb498b291d15f21713811
futo aoi <faoi0803@icloud.com>
2025-05-31 19:47:58 +0900

    [add]SkillBase.csを追加

---
+++ Assets/Scripts/Skill Base.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillBase : MonoBehaviour
+{
+    [SerializeField] public string _SkillName;
+    [SerializeField] public string _Description;
+}
